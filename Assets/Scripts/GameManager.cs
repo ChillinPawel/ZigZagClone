76647c4494b1c763e2fbee8cc9ba03da5b0e5fa2
ChillinPawel <pawel.jarubas@gmail.com>
2021-03-23 23:10:00 +0100

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameStarted;
+    public int score;
+    public Text scoreText;
+    public Text highScoreText;
+
+    private void Awake()
+    {
+        highScoreText.text = "HighScore: " + GetHighScore().ToString();
+    }
+
+    public void StartGame()
+    {
+        gameStarted = true;
+        FindObjectOfType<Road>().StartBuilding();
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            StartGame();
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+
+        if (score > GetHighScore()) {
+            PlayerPrefs.SetInt("Highscore", score);
+            highScoreText.text = "Highscore: " + score.ToString();
+        }
+    }
+
+    public int GetHighScore()
+    {
+        return PlayerPrefs.GetInt("Highscore");
+    }
+}
